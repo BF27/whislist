1ddcd4fd430b7ffc89955db0d17c3ce70fb3f0de
Farkas Bence <farkas.bence27@gmail.com>
2023-10-26 11:07:46 +0200

    create IInputField Component

---
+++ src/lib/components/inputField.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let id: string;
+	export let name: string;
+	export let type: string;
+	export let placeholder: string;
+</script>
+
+<label for={id}>
+	<input
+		{id}
+		{name}
+		{type}
+		{placeholder}
+		class="w-full p-2 border-2 border-black rounded-lg outline-0 shadow-box focus:shadow-none"
+	/>
+</label>
