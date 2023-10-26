7a65833dbfd1524e281aa47c42706628332564e2
Farkas Bence <farkas.bence27@gmail.com>
2023-10-26 11:08:17 +0200

    create layout and pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,6 @@
+<script>
+	import Button from '$lib/components/button.svelte';
+</script>
+
 <h1 class="text-3xl font-bold underline">Hello World!</h1>
+<Button text={'text'} bgColor={'accent'} handler={() => console.log('clicked')} />
