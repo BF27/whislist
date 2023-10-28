3c25c7e97aa7e8815e47384a3336559610677867
Bence Farkas <farkas.bence27@gmail.com>
2023-10-28 10:27:54 +0200

    create signup page

---
+++ src/routes/(auth)/signup/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import SignupForm from "./components/signupForm.svelte";
+
+</script>
+<div class="flex flex-col gap-4 border-2 outline-2 rounded-lg border-black shadow-box p-4 bg-white">
+	<header>
+		<h2 class="font-semibold uppercase text-center">Sign up and make your first wishlist</h2>
+	</header>
+	<SignupForm />
+</div>
