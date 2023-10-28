f1c2cf2f5c0b1b592933042f74e7cbe831eb295b
Bence Farkas <farkas.bence27@gmail.com>
2023-10-28 16:42:58 +0200

    edit login form

--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -1,6 +1,7 @@
 <script>
-	import SignUpForm from "./components/signUpForm.svelte";
+	import SignUpForm from './components/signUpForm.svelte';
 </script>
+
 <div class="flex flex-col gap-4 border-2 outline-2 rounded-lg border-black shadow-box p-4 bg-white">
 	<header>
 		<h2 class="font-semibold uppercase text-center">Sign up and make your first wishlist</h2>
