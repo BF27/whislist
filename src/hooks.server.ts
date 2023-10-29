204dc08faab63ada1eaec6ec8de34683309c5c4a
Bence Farkas <farkas.bence27@gmail.com>
2023-10-29 21:13:19 +0100

    add supabase adapter

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,16 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
+import { SupabaseAdapter } from '@auth/supabase-adapter';
 import GitHub from '@auth/core/providers/github';
 import Google from '@auth/core/providers/google';
 import {
 	GITHUB_ID,
 	GITHUB_SECRET,
 	GOOGLE_CLIENT_ID,
-	GOOGLE_CLIENT_SECRET
+	GOOGLE_CLIENT_SECRET,
+	SUPABASE_SERVICE_ROLE_KEY,
+
 } from '$env/static/private';
+import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 
 export const handle = SvelteKitAuth({
 	providers: [
@@ -18,5 +22,9 @@ export const handle = SvelteKitAuth({
 			clientId: GOOGLE_CLIENT_ID,
 			clientSecret: GOOGLE_CLIENT_SECRET
 		})
-	]
+	],
+	adapter: SupabaseAdapter({
+		url: PUBLIC_SUPABASE_URL,
+		secret: SUPABASE_SERVICE_ROLE_KEY
+	})
 });
